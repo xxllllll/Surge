3c8a656130daca391fdef969b7f82c28f43aeef9
xxllllll <44605747+xxllllll@users.noreply.github.com>
2022-02-03 00:17:04 +0800

    Update and rename IP_bili.js to IP_bilii.js

--- IP_bilii.js
+++ IP_bilii.js
@@ -53,7 +53,7 @@ emoji=City_ValidCheck(obj['province']) == "香港"? "🇭🇰️":emoji
 emoji=City_ValidCheck(obj['province']) == "澳门"? "️🇲🇴️":emoji
 // emoji=City_ValidCheck(obj['province']) == "台湾"? "️️🇹🇼":emoji
 var title =  emoji +'『'+ City_ValidCheck(obj['province'])+'』';//+Area_check(obj['country']);
-var subtitle =  obj['addr'] + ISP_ValidCheck(obj['isp']);
+var subtitle =  '  ' + obj['addr'] + '  ' + ISP_ValidCheck(obj['isp']);
 var ip = obj['addr']; 
 var description = '服务商:'+obj['isp'] + '\n'+'定位: [' +obj["latitude"]+","+obj["longitude"]+"]"+ '\n' + 'IP:'+ obj['addr'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
