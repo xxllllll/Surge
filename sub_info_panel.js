93cc3bd02d218ddf652fa4dbcc5e4c2664926150
xxllllll <44605747+xxllllll@users.noreply.github.com>
2022-03-15 11:18:38 +0800

    Update sub_info_panel.js

--- sub_info_panel.js
+++ sub_info_panel.js
@@ -21,20 +21,9 @@ let args = getArgs();
 (async () => {
   let info = await getDataInfo(args.url);
   if (!info) $done();
-  #let resetDayLeft = getRmainingDays(parseInt(args["reset_day"]));
-
   let used = info.download + info.upload;
   let total = info.total;
-  #let expire = args.expire || info.expire;
   let content = [`用量：${bytesToSize(used)} | ${bytesToSize(total)}`];
-
-  if (resetDayLeft) {
-    content.push(`重置：剩余${resetDayLeft}天`);
-  }
-  if (expire) {
-    if (/^[\d.]+$/.test(expire)) expire *= 1000;
-    content.push(`到期：${formatTime(expire)}`);
-  }
 
   let now = new Date();
   let hour = now.getHours();
