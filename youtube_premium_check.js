861a5946b6474b5229fd8288826fd41320d5620d
xxllllll <44605747+xxllllll@users.noreply.github.com>
2022-04-02 10:08:14 +0800

    Update youtube_premium_check.js

--- youtube_premium_check.js
+++ youtube_premium_check.js
@@ -26,7 +26,7 @@ const BASE_URL = 'https://www.youtube.com/premium'
 
 const DEFAULT_OPTIONS = {
   title: 'YouTube 解锁检测',
-  availableContent: '支持 Premium ➟ #REGION_FLAG# #REGION_NAME#',
+  availableContent: '支持 Premium ➟ #REGION_FLAG# #REGION_NAME_EN#',
   availableIcon: '',
   availableIconColor: '',
   availableStyle: 'good',
